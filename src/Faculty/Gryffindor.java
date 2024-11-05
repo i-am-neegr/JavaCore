0d5972978f694318be439d8952f3b70d4f989157
i-am-neegr <martial.artem@gmail.com>
2024-11-05 17:57:03 +0300

    small fixes

--- src/Faculty/Gryffindor.java
+++ src/Faculty/Gryffindor.java
@@ -2,10 +2,13 @@ package Faculty;
 
 import Student.GryffindorStudent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Gryffindor {
     public String[] GryffindorQualities = {"благородство", "честь", "храбрость"};
 
-    public void StrongestGryffindorStudent(GryffindorStudent student1, GryffindorStudent student2) {
+    public void strongestGryffindorStudent(GryffindorStudent student1, GryffindorStudent student2) {
         int student1Counter = 0;
         int student2Counter = 0;
 
@@ -19,6 +22,22 @@ public class Gryffindor {
         } else {
             System.out.println(student2.getName() + " лучший Гриффиндорец, чем " + student1.getName());
         }
+    }
+    public void strongestGryffindorStudentByArray(GryffindorStudent[] students) {
+        Map<String, int[]> results = new HashMap<>();
+        String[] names = new String[students.length];
+
+        for (GryffindorStudent student : students) {
+            String name = student.getName();
+            int facultativeStrength = 0;
+            int brutalStrength = student.getIntelligenceStrength() + student.getTransgressionDistance();
+
+            for (String quality : GryffindorQualities) {
+                facultativeStrength += student.getQualities().get(quality);
+            }
+            results.put(name, new int[]{brutalStrength, facultativeStrength});
+        }
+
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,15 @@ public class Main {
     public static void main(String[] args) {
 
         GryffindorStudent student1 = new GryffindorStudent("гарик", "потр", 10, 10, 10, 2, 1);
-        student1.printInfo();
-
         HufflepuffStudent student2 = new HufflepuffStudent("захар", "смит", 10, 10, 10, 2, 1);
-        student2.printInfo();
-
         RavenclawStudent student3 = new RavenclawStudent("чжау", "чонг", 4, 5, 9, 2, 3, 1);
-        student3.printInfo();
-
         SlytherinStudent student4 = new SlytherinStudent("дракула", "малой", 2, 2, 2, 2, 2, 2, 4);
-        student4.printInfo();
 
         GryffindorStudent student11 = new GryffindorStudent("героина", "рэйнджер", 9, 9, 6, 3, 2);
-        student11.printInfo();
+
 
         Gryffindor gryffindor = new Gryffindor();
-        gryffindor.StrongestGryffindorStudent(student11, student1);
+        gryffindor.strongestGryffindorStudent(student11, student1);
 
     }
 }
